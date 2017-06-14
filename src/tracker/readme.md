b003b3ed7b156f8109e40557ac6a050fa67e55f5
Tze-Ping Low <lowspin@gmail.com>
2017-06-14 17:39:10 -0400

    initial commit - skeleton with placeholders

--- src/tracker/readme.md
+++ src/tracker/readme.md
@@ -1,1 +1,36 @@
-# todo
+# Function
+
+[Work-In-Progress] Tracker Node for ROS framework to enable real-time processing of ROS bags in Udacity's Didi Chuxing Challenge.
+
+# Overview
+
+* Tracker node subscribes to `MarkerArray` messages from **detector node**, which contains bounding boxes for a single frame
+* Tracker also subscribes to camera `/image_raw` messages for time-stamps
+* Hungarian and Kalman updates are called every time we have a new bounding box message
+* Hungarian algorithm is responsible for calling Kalman filter updates and separate targets into individual tracks.
+
+# Instructions
+
+* Copy the entire `src` folder to your catkin workspace. For example,
+```
+cp -r src ~/catkin_ws/
+cd ~/catkin_ws/
+```
+
+* Note: Make sure `~/catkin_ws/src/tracker/scripts/tracker.py` is executable (use  `chmod +x`).
+
+* Build the catkin package
+```
+catkin_make
+source ~/catkin_ws/devel/setup.bash
+``` 
+
+# To run
+
+* Run detector node. For testing (until detector node is publishing the bbox messages), run [Prerit's code](https://github.com/bostondiditeam/MV3D/tree/master/utils/ROSviz) for tracklet vizualization.
+
+* Then run tracker node:
+
+```rosrun tracker tracker.py```
+
+---
---
+++ src/tracker/scripts/tracker.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from visualization_msgs.msg import Marker,MarkerArray
+from sensor_msgs.msg import Image
+
+class Tracker:
+
+    def __init__(self):
+        self.current_time = rospy.Time()
+        self.latestframetime = 0
+        self.trans = [None]*3
+
+    def kalman_update(self, trans, trackid):
+        print 'Kalman update #', trackid
+        #########################################################
+        #TODO: Kalman filter measurement update for single target
+        #########################################################
+
+    def hungarian_update(self, translist):
+        print 'Hungarian update'
+        #########################################################
+        #TODO: Hungarian algorithm update to separate targets
+        #########################################################
+
+        trackid = 0
+        for trans in translist:
+            self.kalman_update(trans, trackid)
+            trackid += 1
+
+    def handle_image_msg(self, msg):
+        self.latestframetime = msg.header.stamp.to_nsec()
+        print 'latest camera frame time = ', self.latestframetime
+
+    def handle_bbox_msg(self,msg):
+        print 'new bbox message'
+
+        translist = []
+        index = 0
+        bboxtime = msg.markers[0].header.stamp
+        for m in msg.markers:
+
+            trans = m.pose.position
+
+            tx = trans.x
+            ty = trans.y
+            tz = trans.z
+
+            if  m.header.stamp == bboxtime:
+                print 'bbox ',m.id, ' time = ', m.header.stamp, ': Pos = [',tx,',',ty,',',tz,']'
+                translist.append(trans)
+                bboxtime = m.header.stamp
+                index += 1
+
+        print 'Total bbox in this frame = ', index
+        self.hungarian_update(translist)
+
+
+    def startlistening(self):
+        rospy.init_node('tracker', anonymous=True)
+        rospy.Subscriber('/image_raw', Image, self.handle_image_msg)
+        rospy.Subscriber("/bbox", MarkerArray, self.handle_bbox_msg)
+        print 'tracker node initialzed'
+        rospy.spin()
+
+if __name__ == "__main__":
+    tracker = Tracker()
+    tracker.startlistening()
