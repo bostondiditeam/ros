7fea542d6f5ec2a87ef2da5ed04eea8587a0b434
liufeng2012 <liufengsuper2012@126.com>
2017-06-28 20:59:41 +0800

    Add c_python_lib

--- src/sensor/c_python_lib/predict.py
+++ src/sensor/c_python_lib/predict.py
@@ -1,64 +1,59 @@
 import os
 import numpy as np
-# import cv2
+import cv2
 import sys
 import time
 
-sys.path.append(os.path.join(sys.path[0],"../../detector/MV3D/src"))
-sys.path.append(os.path.join(sys.path[0],"../../detector/scripts"))
+sys.path.append("/ext2/didigithub/ros/src/detector/MV3D/src")
+sys.path.append("/ext2/didigithub/ros/src/detector/scripts")
 
-#from config import cfg
-#import mv3d
-#import py2utils
+from config import cfg
+import mv3d
+import py2utils
 
-# global predict
-# global front
 
-# def init():
-#     global predict
-#     global front
-#
-#     tag = "v0.2.1"
-#     print("init the network ", tag)
-#     LIDAR_TOP_SHAPE = (500, 300, 15) # to be decided (400, 400, 8)
-#     LIDAR_FRONT_SHAPE = (0,)  # to be decided.
-#     RGB_SHAPE = (596, 1368, 3)  # to be decided (1096, 1368, 3)
-#
-#     # predict = mv3d.Predictor(LIDAR_TOP_SHAPE, LIDAR_FRONT_SHAPE, RGB_SHAPE, log_tag=tag)
-#     front = np.zeros((0,), dtype=np.float32)
-#
-# def testCase():
-#     global predict
-#     global front
-#
-#     print("testing predict:")
-#     dir = os.path.join(cfg.RAW_DATA_SETS_DIR, "suburu_driving_past_it/suburu07")
-#     rgb_path = os.path.join(dir, "image_02/data", "1492888603962510690.png")
-#     print(rgb_path)
-#     rgb = py2utils.crop_image(cv2.imread(rgb_path))
-#     print(rgb.shape)
-#     lidar_path = os.path.join(dir, "velodyne_points/data", "1492888603962510690.bin")
-#     lidar =np.fromfile(lidar_path, np.float32)
-#     lidar = lidar.reshape((-1, 4))
-#     top = py2utils.g_lidar_to_top(lidar)
-#
-#     start = time.time()
-#     boxes3d, probs = predict([top], [front], [rgb])
-#     timestamp = time.time()-start
-#     print("predict boxes len=%d use %f seconds" % (len(boxes3d),timestamp))
+tag = "v0.2.1"
+print("init the network ", tag)
+print(sys.version)
 
-def predict(top : np.array, rgb : np.array):
-    #start = time.time()
-    # boxes3d, probs = predict([top], [front], [rgb])
-    # print("predict boxes len: {} use time {}".format(boxes3d.shape[0], time.time() - start))
-    # return boxes3d.tolist()
-    print("call python predict")
+LIDAR_TOP_SHAPE = (500, 300, 15) # to be decided (400, 400, 8)
+LIDAR_FRONT_SHAPE = (0,)  # to be decided.
+RGB_SHAPE = (596, 1368, 3)  # to be decided (1096, 1368, 3)
+
+predict = mv3d.Predictor(LIDAR_TOP_SHAPE, LIDAR_FRONT_SHAPE, RGB_SHAPE, log_tag=tag)
+front = np.zeros((0,), dtype=np.float32)
+
+def predict_func(top, rgb):
+    print("call python predict: top:{} rgb:{}".format(top.shape, rgb.shape))
+    #assert(top.shape == LIDAR_TOP_SHAPE)
+    #assert(rgb.shape == RGB_SHAPE)
+    start = time.time()
+    boxes3d, probs = predict([top], [front], [rgb])
+    print("predict boxes len: {} use time {}".format(boxes3d.shape[0], time.time() - start))
+    return boxes3d
+
+# Testing self
+print("===== begin testing predict:")
+dir = os.path.join(cfg.RAW_DATA_SETS_DIR, "suburu_driving_past_it/suburu07")
+rgb_path = os.path.join(dir, "image_02/data", "1492888603962510690.png")
+print(rgb_path)
+rgb = py2utils.crop_image(cv2.imread(rgb_path))
+print(rgb.shape)
+lidar_path = os.path.join(dir, "velodyne_points/data", "1492888603962510690.bin")
+lidar =np.fromfile(lidar_path, np.float32)
+lidar = lidar.reshape((-1, 4))
+top = py2utils.g_lidar_to_top(lidar)
+boxes3d = predict_func(top, rgb)
+print("===== end testing predict:")
+
+def predict_test(top : np.array, rgb : np.array):
     print(top.shape)
     print(rgb.shape)
-
-    print("-------------")
+    print("================================")
     print(top)
-    #print(rgb)
+    print("================================")
+    print(rgb)
+    return top
 
 def func(_a, _b, _c):
     print(_a.shape)
@@ -74,6 +69,3 @@ def func(_a, _b, _c):
     print(y.shape)
     print(y)
     return _c ** _d
-
-#init()
-#testCase()
--- src/sensor/c_python_lib/predict_test.cpp
+++ src/sensor/c_python_lib/predict_test.cpp
@@ -7,26 +7,40 @@
 #include <iostream>
 using namespace std;
 
+// const int TOP_SHAPE_0 = 500;
+// const int TOP_SHAPE_1 = 300;
+// const int TOP_SHAPE_2 = 15;
+// const int RGB_SHAPE_0 = 596;
+// const int RGB_SHAPE_1 = 1368;
+// const int RGB_SHAPE_2 = 3;
+
+const int TOP_SHAPE_0 = 3;
+const int TOP_SHAPE_1 = 3;
+const int TOP_SHAPE_2 = 3;
+const int RGB_SHAPE_0 = 3;
+const int RGB_SHAPE_1 = 3;
+const int RGB_SHAPE_2 = 3;
+
 int init_numpy(){
     import_array();
 }
 
-double*** create3DArray(int nx, int ny, int nz) {
-    double*** p = new double**[nx];
+float*** create3DArray(int nx, int ny, int nz) {
+    float*** p = new float**[nx];
     for(int i = 0; i < nx; i ++) {
-        p[i] = new double*[ny];
+        p[i] = new float*[ny];
         for(int j = 0; j < ny; j ++) {
-            p[i][j] = new double[nz];
+            p[i][j] = new float[nz];
             for(int k = 0; k < nz; k ++) {
                 p[i][j][k] = (100.0*i + 10.0*j + k);
-                cout << p[i][j][k] << endl;
+                //cout << p[i][j][k] << endl;
             }
         }
     }
     return p;
 }
 
-void free3DArray(double*** p, int nx, int ny) {
+void free3DArray(float*** p, int nx, int ny) {
     for(int i = 0; i < nx; i ++) {
         for(int j = 0; j < ny; j ++)
             delete[] p[i][j];
@@ -62,28 +76,34 @@ int main(int argc, char* argv[])
 	//PyObject *pList=NULL;
 
 	cout<< " call : "<<endl;
-	pFunc = PyObject_GetAttrString(pModule, "predict");
+	pFunc = PyObject_GetAttrString(pModule, "predict_test");
 	pArgs=PyTuple_New(2);
 
-	double*** topArrays = create3DArray(3, 3, 3);
-	double*** rgbArrays = create3DArray(3, 3, 3);
+	float*** topArrays = create3DArray(TOP_SHAPE_0, TOP_SHAPE_1, TOP_SHAPE_2);
+	float*** rgbArrays = create3DArray(RGB_SHAPE_0, RGB_SHAPE_1, RGB_SHAPE_2);
 
-    npy_intp  topDims[3] = {3, 3, 3};
-    npy_intp  rgbDims[3] = {3, 3, 3};
+	cout << topArrays[1][1][1] << endl;
+
+    npy_intp  topDims[3] = {TOP_SHAPE_0, TOP_SHAPE_1, TOP_SHAPE_2};
+    npy_intp  rgbDims[3] = {RGB_SHAPE_0, RGB_SHAPE_1, RGB_SHAPE_2};
 
 	cout << "init ok" << endl;
 
-	PyObject *pa  = PyArray_SimpleNewFromData(3, topDims, NPY_DOUBLE, topArrays);
-	PyObject *pb  = PyArray_SimpleNewFromData(3, rgbDims, NPY_DOUBLE, rgbArrays);
+	PyObject *pa  = PyArray_SimpleNewFromData(3, topDims, NPY_FLOAT, topArrays);
+	PyObject *pb  = PyArray_SimpleNewFromData(3, rgbDims, NPY_FLOAT, rgbArrays);
 
 	PyTuple_SetItem(pArgs, 0, pa);
 	PyTuple_SetItem(pArgs, 1, pb);
 
-	free3DArray(topArrays, 3, 3);
-	free3DArray(rgbArrays, 3, 3);
+	cout << topArrays[1][1][1] << endl;
 
 	pValue = PyObject_CallObject(pFunc, pArgs);
 	cout<< " \n ====================\n result : "<<endl;
+
+	cout << topArrays[1][1][1] << endl;
+
+	free3DArray(topArrays, TOP_SHAPE_0, TOP_SHAPE_1);
+	free3DArray(rgbArrays, RGB_SHAPE_0, RGB_SHAPE_1);
 
 	Py_Finalize();
 
