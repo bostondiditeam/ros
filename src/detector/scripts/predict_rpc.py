faeb47f7c2119109ee475e4ae88f1935a3acbb9d
XueFengZeng <t-ink@qq.com>
2017-06-30 15:52:18 +0800

    Use MV3D builtin function instead of py2utils.py.

--- src/detector/scripts/predict_rpc.py
+++ src/detector/scripts/predict_rpc.py
@@ -2,6 +2,7 @@ from twisted.web import xmlrpc, server
 import numpy as np
 import argparse
 import os
+os.environ['DISPLAY']=':0'
 import numpy as np
 import cv2
 import sys
@@ -10,9 +11,10 @@ import time
 sys.path.append(os.path.join(sys.path[0],"../MV3D/src"))
 from config import cfg
 import mv3d
+import data
 from net.processing.boxes3d import boxes3d_decompose
-from data import Preprocess
-import py2utils
+from data import preprocess
+
 
 parser = argparse.ArgumentParser(description='predict_rpc')
 parser.add_argument('-n', '--tag', type=str, nargs='?', default='unknown_tag',
@@ -29,7 +31,7 @@ LIDAR_TOP_SHAPE = (500, 300, 15) # to be decided (400, 400, 8)
 LIDAR_FRONT_SHAPE = (0,)  # to be decided.
 RGB_SHAPE = (596, 1368, 3)  # to be decided (1096, 1368, 3)
 
-process = Preprocess()
+
 predict = mv3d.Predictor(LIDAR_TOP_SHAPE, LIDAR_FRONT_SHAPE, RGB_SHAPE, log_tag=tag)
 front = np.zeros((0, ), dtype=np.float32)
 
@@ -37,14 +39,14 @@ def testCase():
     print("testing predict:")
     dir = os.path.join(cfg.RAW_DATA_SETS_DIR, "suburu_driving_past_it/suburu07")
     rgb_path = os.path.join(dir, "image_02/data", "1492888603962510690.png")
-    rgb = process.rgb(cv2.imread(rgb_path))
+    rgb = preprocess.rgb(cv2.imread(rgb_path))
     print(rgb.shape)
     lidar_path = os.path.join(dir, "velodyne_points/data", "1492888603962510690.bin")
     lidar =np.fromfile(lidar_path, np.float32)
     lidar = lidar.reshape((-1, 4))
-    top = py2utils.g_lidar_to_top(lidar)
+    top = preprocess.lidar_to_top(lidar)
     if 0: # test top is ok?
-        top_image = py2utils.draw_top_image(top)
+        top_image = data.draw_top_image(top)
         cv2.imshow("top", top_image)
         cv2.waitKey(10000)
 
